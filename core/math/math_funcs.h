e5b533b481f0395591abd7dbfdebd5a02e0f075b
nazarii <nazarii.yablonskyi.pp.2022@lpnu.ua>
2024-05-13 00:11:34 +0300

    test2

--- core/math/math_funcs.h
+++ core/math/math_funcs.h
@@ -387,6 +387,18 @@ public:
 		return omt * (p_start * omt2 + 3.0 * p_control_2 * t2) + 3.0 * p_control_1 * omt2 * p_t + p_end * t3;
 	}
 
+#ifdef _M_ARM64
+	static _ALWAYS_INLINE_ float bezier_interpolate(float p_start, float p_control_1, float p_control_2, float p_end, float p_t) {
+		/* Formula from Wikipedia article on Bezier curves. */
+		float omt = (1.0f - p_t);
+		float omt2 = omt * omt;
+		float omt3 = omt2 * omt;
+		float t2 = p_t * p_t;
+		float t3 = t2 * p_t;
+		return p_start * omt3 + p_control_1 * omt2 * p_t * 3.0f + p_control_2 * omt * t2 * 3.0f + p_end * t3;
+	}
+
+#else
 	static _ALWAYS_INLINE_ float bezier_interpolate(float p_start, float p_control_1, float p_control_2, float p_end, float p_t) {
 		/* Formula from Wikipedia article on Bezier curves. */
 		float omt = (1.0f - p_t);
@@ -396,6 +408,7 @@ public:
 
 		return omt * (p_start * omt2 + 3.0f * p_control_2 * t2) + 3.0f * p_control_1 * omt2 * p_t + p_end * t3;
 	}
+#endif
 
 	static _ALWAYS_INLINE_ double bezier_derivative(double p_start, double p_control_1, double p_control_2, double p_end, double p_t) {
 		/* Formula from Wikipedia article on Bezier curves. */
