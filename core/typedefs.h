979730c70921ccc4aff635eb11cbadaa611ed1f8
Nazarii <nazarii.yablonskyi.pp.2022@lpnu.ua>
2024-09-17 14:44:44 +0300

    Use inline in ALWAYS and FORCE_INLINE

--- core/typedefs.h
+++ core/typedefs.h
@@ -54,25 +54,10 @@ static_assert(__cplusplus >= 201703L);
 #define _MKSTR(m_x) _STR(m_x)
 #endif
 
-// Should always inline no matter what.
-#ifndef _ALWAYS_INLINE_
-#if defined(__GNUC__)
-#define _ALWAYS_INLINE_ __attribute__((always_inline)) inline
-#elif defined(_MSC_VER)
-#define _ALWAYS_INLINE_ __forceinline
-#else
+// Should always inline no matter what. // TODO fix comment if I will deside to make PR!!!!!!!!!!
 #define _ALWAYS_INLINE_ inline
-#endif
-#endif
-
-// Should always inline, except in dev builds because it makes debugging harder.
-#ifndef _FORCE_INLINE_
-#ifdef DEV_ENABLED
-#define _FORCE_INLINE_ inline
-#else
 #define _FORCE_INLINE_ _ALWAYS_INLINE_
-#endif
-#endif
+
 
 // In some cases [[nodiscard]] will get false positives,
 // we can prevent the warning in specific cases by preceding the call with a cast.
